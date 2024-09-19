843aa84ca5409ffea106bc1b7c191e8bb1d48623
salmaakhalifa199 <salmakhalifa06991@gmail.com>
2024-09-19 04:17:20 +0300

    MVC07

---
+++ Company.Services/Helper/EmailSettings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Services.Helper
+{
+    public static class EmailSettings
+    {
+        public static void SendEmail(Email input)
+        {
+            var client = new SmtpClient("smtp.gmail.com", 587);
+
+            client.EnableSsl = true;
+
+            client.Credentials = new NetworkCredential("salma.sherif1992003@gmail.com", "gqjmbvmqktwaorqe");
+
+            client.Send("salma.sherif1992003@gmail.com" , input.To,input.Subject,input.Body);
+            // from = "salma.sherif1992003@gmail.com"
+        }
+        // smtp da protocol bnshtghdmo 34an nagem el gmail (ab3t email)
+    }
+}
--- Company.Web/Program.cs
+++ Company.Web/Program.cs
@@ -104,7 +104,7 @@ namespace Company.Web
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Account}/{action=SignUp}");
 
             app.Run();
         }
